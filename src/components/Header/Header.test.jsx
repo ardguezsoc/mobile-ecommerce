3b49d4dba7e17809aaadf37d1ee7fc47d86f2286
ardguezsoc <adevsocorro@gmail.com>
2023-07-13 16:09:05 +0200

    test: adding react testing library to remarkable components

---
+++ src/components/Header/Header.test.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { it, describe, vi, beforeEach, expect } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Header } from './Header';
+import { useNavigate } from 'react-router-dom';
+import { useCart } from '../../hooks';
+
+vi.mock('react-router-dom', () => ({
+  useNavigate: vi.fn(),
+}));
+
+vi.mock('../../hooks', () => ({
+  useCart: vi.fn(),
+}));
+
+describe('Header', () => {
+  const navigateMock = vi.fn();
+
+  beforeEach(() => {
+    useNavigate.mockReturnValue(navigateMock);
+    useCart.mockReturnValue({
+      numberOfItems: 5,
+    });
+  });
+
+  it('Renders the header correctly', () => {
+    render(<Header />);
+
+    const logoImage = screen.getByAltText('logo');
+    const cartImage = screen.getByAltText('cart');
+    const webName = screen.getByText('EMobile');
+    const cartNumberItems = screen.getByText('5');
+
+    expect(logoImage).toBeInTheDocument();
+    expect(cartImage).toBeInTheDocument();
+    expect(webName).toBeInTheDocument();
+    expect(cartNumberItems).toBeInTheDocument();
+  });
+
+  it('Calls the navigate function when the header logo is clicked', () => {
+    render(<Header />);
+
+    const headerLogo = screen.getByAltText('logo');
+    fireEvent.click(headerLogo);
+
+    expect(navigateMock).toHaveBeenCalledWith('/');
+  });
+
+  it('calls the navigate function when the web name is clicked', () => {
+    render(<Header />);
+
+    const webName = screen.getByText('EMobile');
+    fireEvent.click(webName);
+
+    expect(navigateMock).toHaveBeenCalledWith('/');
+  });
+});
--- src/components/MobileCardList/MobileCardList.jsx
+++ src/components/MobileCardList/MobileCardList.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Card } from '../Card';
 import './index.css';
 
-export const MobileCardList = ({ mobileList }) => {
+export const MobileCardList = ({ mobileList, navigateTo }) => {
   return (
     <div className="wrapperGrid">
       {mobileList.map((mobileItem) => (
-        <Card key={mobileItem.id} mobileItem={mobileItem} />
+        <Card key={mobileItem.id} mobileItem={mobileItem} navigateTo={navigateTo} />
       ))}
     </div>
   );
